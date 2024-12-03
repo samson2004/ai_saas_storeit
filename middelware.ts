17e22b8b11446ea505528a20d36f7d3ba75d7199
samson <samsonzacharia1973@gmail.com>
2024-12-03 11:02:45 +0530

    v1.11

--- middelware.ts
+++ middelware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/api/webhooks/clerk(.*)','/sign-in(.*)','/sign-up(.*)',])
+const isProtectedRoute = createRouteMatcher(['/app/api/webhooks/clerk(.*)','/sign-in(.*)','/sign-up(.*)',])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
